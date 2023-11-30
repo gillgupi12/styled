ee006b49801112598065080780817e960ea90ef4
gurpindersinghgill <gupigill12@gmail.com>
2023-11-30 15:28:56 +0800

    feat: remove redunant code

--- pages/index.vue
+++ pages/index.vue
@@ -2,78 +2,53 @@
 import gsap from "gsap";
 
 onMounted(() => {
+ 
 
-  function isInViewport(el: Element) {
-          const rect = el.getBoundingClientRect();
-          return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <=
-              (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <=
-              (window.innerWidth || document.documentElement.clientWidth)
-          );
-        }
-        const rows = document.querySelectorAll(".row");
-        rows.forEach((row) => {
-          if (isInViewport(row)) {
-            const img = row.querySelector("img");
-            if (row.querySelector(".left")) {
-              gsap.to(img, {
-                clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-              });
-            } else {
-              gsap.to(img, {
-                clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-              });
-            }
-          }
-        });
+  (gsap.utils.toArray(".img-container.right img") as HTMLElement[]).forEach(
+    (img: HTMLElement) => {
+      gsap.to(img, {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        scrollTrigger: {
+          trigger: img,
+          start: "top 75%",
+          end: "bottom 70%",
+          scrub: true,
+        },
+      });
+    }
+  );
 
-        (gsap.utils.toArray(".img-container.right img") as HTMLElement[]).forEach(
-        (img: HTMLElement) => {
-          gsap.to(img, {
-            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-            scrollTrigger: {
-              trigger: img,
-              start: "top 75%",
-              end: "bottom 70%",
-              scrub: true,
-            },
-          });
-        });
+  (gsap.utils.toArray(".img-container.left img") as HTMLElement[]).forEach(
+    (img: HTMLElement) => {
+      gsap.to(img, {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        scrollTrigger: {
+          trigger: img,
+          start: "top 75%",
+          end: "bottom 70%",
+          scrub: true,
+        },
+      });
+    }
+  );
 
-        (gsap.utils.toArray(".img-container.left img") as HTMLElement[]).forEach(
-        (img: HTMLElement) => {
-          gsap.to(img, {
-            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-            scrollTrigger: {
-              trigger: img,
-              start: "top 75%",
-              end: "bottom 70%",
-              scrub: true,
-            },
-          });
-        });
-
-        (gsap.utils.toArray(".img-container p")as HTMLElement[]).forEach((text) => {
-          gsap.from(text, {
-            opacity: 0,
-            y: 20,
-            scrollTrigger: {
-              trigger: text,
-              start: "top 90%",
-              toggleActions: "play none none reverse",
-            },
-          });
-        });
-
+  (gsap.utils.toArray(".img-container p") as HTMLElement[]).forEach((text) => {
+    gsap.from(text, {
+      opacity: 0,
+      y: 20,
+      scrollTrigger: {
+        trigger: text,
+        start: "top 90%",
+        toggleActions: "play none none reverse",
+      },
+    });
+  });
 });
 </script>
 
 <template>
- <div>
-  <div class="header"> </div>
+  <div >
+    <div class="header"> </div>
 
     <div class="container">
       <div class="row" id="row-1">
@@ -88,7 +63,7 @@ onMounted(() => {
       <div class="row" id="row-2">
         <div class="col"></div>
         <div class="col">
-          <div class=" canvas img-container left">
+          <div class="canvas img-container left">
             <img src="/1.jpg" alt="" />
             <p>Futuristic control relic</p>
           </div>
@@ -96,7 +71,7 @@ onMounted(() => {
       </div>
       <div class="row" id="row-3">
         <div class="col">
-          <div class=" canvas img-container right">
+          <div class="canvas img-container right">
             <img src="/2.jpg" alt="" />
             <p>Neo-Tokyo crimson glow</p>
           </div>
@@ -106,7 +81,7 @@ onMounted(() => {
       <div class="row" id="row-4">
         <div class="col"></div>
         <div class="col">
-          <div class=" canvas img-container left">
+          <div class="canvas img-container left">
             <img src="/3.jpg" alt="" />
             <p>Galactic ocean abyss</p>
           </div>
@@ -114,7 +89,7 @@ onMounted(() => {
       </div>
       <div class="row" id="row-5">
         <div class="col">
-          <div class=" canvas img-container right">
+          <div class="canvas img-container right">
             <img src="/4.jpg" alt="" />
             <p>Hued cosmic dust</p>
           </div>
@@ -132,7 +107,7 @@ onMounted(() => {
       </div>
       <div class="row" id="row-7">
         <div class="col">
-          <div class=" canvas img-container right">
+          <div class="canvas img-container right">
             <img src="/6.jpg" alt="" />
             <p>Hued cosmic dust</p>
           </div>
@@ -150,7 +125,7 @@ onMounted(() => {
       </div>
       <div class="row" id="row-9">
         <div class="col">
-          <div class=" canvas img-container right">
+          <div class="canvas img-container right">
             <img src="/8.jpg" alt="" />
             <p>Hued cosmic dust</p>
           </div>
@@ -169,12 +144,11 @@ onMounted(() => {
 
       <div class="whitespace"></div>
     </div>
-
- </div>
+  </div>
 </template>
-<style >
+<style>
 :root {
-  --color-bg: #000000;
+  --color-bg: #ffffff;
   --color-text: #ffffff;
 }
 
@@ -194,7 +168,7 @@ body {
 
 .header {
   width: 100%;
-  height:400px;
+  height: 400px;
   padding: 1em;
   display: flex;
   justify-content: space-between;
@@ -295,7 +269,6 @@ img {
   flex: 5;
 }
 
-
 .img-container.right img {
   clip-path: polygon(100% 0, 100% 0, 100% 0, 100% 0);
 }
@@ -303,7 +276,6 @@ img {
 .img-container.left img {
   clip-path: polygon(0 0, 0 0, 0 0, 0 0);
 }
-
 
 .whitespace {
   width: 100%;
